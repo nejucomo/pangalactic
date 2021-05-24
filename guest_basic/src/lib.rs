d5239f1709e7c0613803a95a74c0e0146ed80d43
Pangloss <pangloss@utopia>
2021-05-23 21:56:22 -0700

    Rough draft at a byte buf bridge api.

--- guest_basic/src/lib.rs
+++ guest_basic/src/lib.rs
@@ -1,3 +1,17 @@
+#[link(wasm_import_module = "what_mod_name_should_be_here")]
+extern {
+    fn get_bytes(buf: *mut u8, len: usize);
+}
+
 #[no_mangle]
 pub extern fn main() {
+    let mut v = Vec::with_capacity(8);
+
+    for _ in 0..8 {
+        v.push(0u8);
+    }
+
+    unsafe {
+        get_bytes(v.as_mut_ptr(), 8);
+    }
 }
