0cd076901cb45c17de2a7de38ea21beed4611356
Pangloss <pangloss@utopia>
2021-05-27 20:53:39 -0600

    Use `derive_more` for `pangalactic::Error`.

--- pangalactic/src/error.rs
+++ pangalactic/src/error.rs
@@ -1,18 +1,5 @@
-#[derive(Debug)]
+#[derive(Debug, derive_more::From)]
 pub enum Error {
     IO(std::io::Error),
     WASMI(wasmi::Error),
-}
-
-// FIXME: use derive_more::From
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Error {
-        Error::IO(e)
-    }
-}
-
-impl From<wasmi::Error> for Error {
-    fn from(e: wasmi::Error) -> Error {
-        Error::WASMI(e)
-    }
 }
