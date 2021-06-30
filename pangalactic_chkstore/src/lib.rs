a4942178713942024a8669643bc48b8787131ab1
Pangloss <pangloss@utopia>
2021-06-30 08:12:06 -0700

    Implement a write-then-read test.

--- pangalactic_chkstore/src/lib.rs
+++ pangalactic_chkstore/src/lib.rs
@@ -9,8 +9,26 @@ pub use store::CHKStore;
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use test_case::test_case;
+
+    #[test_case(b"")]
+    #[test_case(b"hello world")]
+    fn store_then_read(contents: &[u8]) -> std::io::Result<()> {
+        use std::io::{Read, Write};
+        use testdir::testdir;
+
+        let store = crate::CHKStore::init(testdir!());
+
+        let mut w = store.open_writer()?;
+        w.write_all(contents)?;
+        let key = w.commit()?;
+
+        let mut r = store.open_reader(key)?;
+        let mut bytes = vec![];
+        r.read_to_end(&mut bytes)?;
+        r.verify()?;
+
+        assert_eq!(bytes, contents);
+        Ok(())
     }
 }
--- pangalactic_chkstore/src/store.rs
+++ pangalactic_chkstore/src/store.rs
@@ -6,9 +6,8 @@ use std::path::PathBuf;
 pub struct CHKStore(PathBuf);
 
 impl CHKStore {
-    pub fn init() -> Result<CHKStore, pangalactic_dirs::InitError> {
-        let datadir = pangalactic_dirs::datadir!()?;
-        Ok(CHKStore(datadir))
+    pub fn init(datadir: PathBuf) -> CHKStore {
+        CHKStore(datadir)
     }
 
     pub fn open_writer(&self) -> std::io::Result<Writer> {
