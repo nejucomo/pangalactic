baa7fbc07abbd4b017cf1a4fd87a7ffeef06123c
Pangloss <pangloss@utopia>
2021-07-05 19:51:38 -0700

    Make `…_dirstore` consistent mod structure.

--- pangalactic_dirstore/src/lib.rs
+++ pangalactic_dirstore/src/lib.rs
@@ -1,34 +1,11 @@
 mod key;
 mod randtoken;
+mod store;
 mod writer;
 
-use pangalactic_store::Store;
 use std::io::Result as IOResult;
-use std::path::PathBuf;
 
-pub struct DirStore(PathBuf);
-
-impl DirStore {
-    pub fn init(datadir: PathBuf) -> DirStore {
-        DirStore(datadir)
-    }
-}
-
-impl Store for DirStore {
-    type Key = self::key::Key;
-    type Reader = std::fs::File;
-    type Writer = self::writer::Writer;
-
-    fn open_writer(&self) -> IOResult<Self::Writer> {
-        Self::Writer::open(&self.0)
-    }
-
-    fn open_reader(&self, key: &Self::Key) -> IOResult<Self::Reader> {
-        use pangalactic_store::StoreKey;
-
-        std::fs::File::open(self.0.join(key.b64_encode()))
-    }
-}
+pub use store::DirStore;
 
 #[test]
 fn test_roundtrip() -> std::io::Result<()> {
---
+++ pangalactic_dirstore/src/store.rs
@@ -0,0 +1,27 @@
+use pangalactic_store::Store;
+use std::io::Result as IOResult;
+use std::path::PathBuf;
+
+pub struct DirStore(PathBuf);
+
+impl DirStore {
+    pub fn init(datadir: PathBuf) -> DirStore {
+        DirStore(datadir)
+    }
+}
+
+impl Store for DirStore {
+    type Key = crate::key::Key;
+    type Reader = std::fs::File;
+    type Writer = crate::writer::Writer;
+
+    fn open_writer(&self) -> IOResult<Self::Writer> {
+        Self::Writer::open(&self.0)
+    }
+
+    fn open_reader(&self, key: &Self::Key) -> IOResult<Self::Reader> {
+        use pangalactic_store::StoreKey;
+
+        std::fs::File::open(self.0.join(key.b64_encode()))
+    }
+}
