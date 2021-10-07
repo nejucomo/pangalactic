f4f16ab298b7b8f00d259ccce0df2fd97629cdaf
Pangloss <pangloss@utopia>
2021-10-07 09:59:12 -0700

    Rename internal mod for `FromGuestValue`.

--- pangalactic_wasmi/src/lib.rs
+++ pangalactic_wasmi/src/lib.rs
@@ -1,13 +1,13 @@
-mod arg;
 mod args;
 mod hostfunc;
 mod resolver;
 mod ret;
 mod table;
+mod value;
 
-pub use self::arg::FromGuestValue;
 pub use self::args::FromGuestArgs;
 pub use self::hostfunc::HostFunc;
 pub use self::resolver::HostFuncResolver;
 pub use self::ret::IntoGuestReturn;
 pub use self::table::{Handle, Table};
+pub use self::value::FromGuestValue;
