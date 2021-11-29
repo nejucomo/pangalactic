92112aceab3e31bf84daa3774fc792de7af25fa6
Pangloss <pangloss@utopia>
2021-11-29 10:56:12 -0800

    [No-TDD] Introduce `Table::release`.

--- pangalactic_wasmi/src/table.rs
+++ pangalactic_wasmi/src/table.rs
@@ -2,9 +2,12 @@
 /// This design is only type safe against context confusion bugs if there is only one table for each type.
 mod handle;
 
-use wasmi::{Trap, TrapKind::TableAccessOutOfBounds};
+use wasmi::{
+    Trap,
+    TrapKind::{ElemUninitialized, TableAccessOutOfBounds},
+};
 
-pub struct Table<T>(Vec<T>);
+pub struct Table<T>(Vec<Option<T>>);
 
 pub use self::handle::Handle;
 
@@ -14,26 +17,46 @@ impl<T> Table<T> {
     }
 
     pub fn insert(&mut self, item: T) -> Handle<T> {
+        for (ix, slot) in self.0.iter_mut().enumerate() {
+            if slot.is_none() {
+                slot.replace(item);
+                return Handle::from(ix);
+            }
+        }
+
+        // No tombstones found, so allocate a new slot:
         let h = Handle::from(self.0.len());
-        self.0.push(item);
+        self.0.push(Some(item));
         h
     }
 
-    pub fn get(&self, h: Handle<T>) -> Result<&T, Trap> {
-        let ix = usize::from(h);
-        if ix < self.0.len() {
-            Ok(&self.0[ix])
+    pub fn release(&mut self, h: Handle<T>) -> Result<(), Trap> {
+        let slot = self.get_slot_mut(h)?;
+        if slot.is_none() {
+            Err(Trap::new(ElemUninitialized))
         } else {
-            Err(Trap::new(TableAccessOutOfBounds))
+            *slot = None;
+            Ok(())
         }
     }
 
-    pub fn get_mut(&mut self, h: Handle<T>) -> Result<&mut T, Trap> {
-        let ix = usize::from(h);
-        if ix < self.0.len() {
-            Ok(&mut self.0[ix])
-        } else {
-            Err(Trap::new(TableAccessOutOfBounds))
-        }
+    pub fn get(&self, h: Handle<T>) -> Result<&T, Trap> {
+        deref_slot(self.get_slot(h)?.as_ref())
     }
+
+    pub fn get_mut(&mut self, h: Handle<T>) -> Result<&mut T, Trap> {
+        deref_slot(self.get_slot_mut(h)?.as_mut())
+    }
+
+    fn get_slot(&self, h: Handle<T>) -> Result<&Option<T>, Trap> {
+        deref_slot(self.0.get(usize::from(h)))
+    }
+
+    fn get_slot_mut(&mut self, h: Handle<T>) -> Result<&mut Option<T>, Trap> {
+        deref_slot(self.0.get_mut(usize::from(h)))
+    }
+}
+
+fn deref_slot<T>(slot: Option<T>) -> Result<T, Trap> {
+    slot.ok_or(Trap::new(TableAccessOutOfBounds))
 }
