39a0d0149e6c424e427a4c3c4356d9a49da60536
Nate Wilcox <nathan+dev@electriccoin.co>
2022-11-11 18:34:44 -0800

    Change `open_reader` to take result; enable `io-util` tokio feature.

--- dagwasm/src/dag/store.rs
+++ dagwasm/src/dag/store.rs
@@ -7,6 +7,6 @@ pub trait Store {
     type Reader: tokio::io::AsyncRead;
     type Writer: Writer<Link = Self::Link>;
 
-    async fn open_reader(&mut self, link: Self::Link) -> Self::Reader;
+    async fn open_reader(&mut self, link: Self::Link) -> anyhow::Result<Self::Reader>;
     async fn open_writer(&mut self) -> Self::Writer;
 }
