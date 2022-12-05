f508dedcd92b6fdd72907a33a5127d4c0da0575e
Nate Wilcox <nathan+dev@electriccoin.co>
2022-12-04 17:34:42 -0700

    Bundle all guest wasms into `dagwasm-guests::get_wasm_bytes`.

---
+++ guests/src/lib.rs
@@ -0,0 +1,17 @@
+use include_dir::{include_dir, Dir};
+
+static WASM_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/guests/target/wasms");
+
+pub fn get_wasm_bytes(name: &str) -> anyhow::Result<&'static [u8]> {
+    let filename = format!("{}.wasm", name);
+    let f = WASM_DIR
+        .get_file(&filename)
+        .ok_or_else(|| anyhow::Error::msg(format!("not found: {:?}", filename)))?;
+    Ok(f.contents())
+}
+
+#[test]
+fn get_wasm_bytes_for_ident() -> anyhow::Result<()> {
+    get_wasm_bytes("ident")?;
+    Ok(())
+}
