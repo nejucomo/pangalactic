e284af03ef52cc53d28de7669338918c3b4ef41f
Nate Wilcox <nathan+dev@electriccoin.co>
2022-12-08 14:25:48 -0700

    Unintegrated guest integration test backend `test_derivation_is_dir`.

--- guests/src/lib.rs
+++ guests/src/lib.rs
@@ -3,8 +3,20 @@ use include_dir::{include_dir, Dir};
 static WASM_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/guests/target/wasms");
 
 pub fn get_wasm_bytes(name: &str) -> anyhow::Result<&'static [u8]> {
+    get_wasm_bytes_from_dir(&WASM_DIR, name)
+}
+
+#[cfg(test_guests)]
+static TEST_WASM_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/guests/target/test-wasms");
+
+#[cfg(test_guests)]
+pub fn get_test_wasm_bytes(name: &str) -> anyhow::Result<&'static [u8]> {
+    get_wasm_bytes_from_dir(&TEST_WASM_DIR, name)
+}
+
+fn get_wasm_bytes_from_dir(dir: &Dir<'static>, name: &str) -> anyhow::Result<&'static [u8]> {
     let filename = format!("{name}.wasm");
-    let f = WASM_DIR
+    let f = dir
         .get_file(&filename)
         .ok_or_else(|| anyhow::Error::msg(format!("not found: {filename:?}")))?;
     Ok(f.contents())
