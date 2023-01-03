70619ac590d6b39b1989e6db65b093cd0736737b
Nate Wilcox <nathan+dev@electriccoin.co>
2023-01-02 20:13:50 -0800

    Impl host API `link_open_file_reader`; integration test `input_is_hello_world` passes.

--- host/src/bytereader.rs
+++ host/src/bytereader.rs
@@ -1,26 +1,45 @@
 use dagwasm_dir::Name;
+use dagwasm_store::Store;
 use std::io::Cursor;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use tokio::io::{AsyncRead, ReadBuf};
 
-#[derive(Debug)]
-pub(crate) struct ByteReader(Cursor<Vec<u8>>);
+pub(crate) enum ByteReader<S>
+where
+    S: Store,
+{
+    Buf(Cursor<Vec<u8>>),
+    Store(<S as Store>::Reader),
+}
 
-impl From<Name> for ByteReader {
+impl<S> From<Name> for ByteReader<S>
+where
+    S: Store,
+{
     fn from(name: Name) -> Self {
-        ByteReader(Cursor::new(name.into_bytes()))
+        ByteReader::Buf(Cursor::new(name.into_bytes()))
     }
 }
 
-impl AsyncRead for ByteReader {
+impl<S> AsyncRead for ByteReader<S>
+where
+    S: Store,
+{
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        let mutself = Pin::into_inner(self);
-        let pincursor = Pin::new(&mut mutself.0);
-        AsyncRead::poll_read(pincursor, cx, buf)
+        match Pin::into_inner(self) {
+            ByteReader::Buf(cursor) => {
+                let pin = Pin::new(cursor);
+                AsyncRead::poll_read(pin, cx, buf)
+            }
+            ByteReader::Store(reader) => {
+                let pin = Pin::new(reader);
+                AsyncRead::poll_read(pin, cx, buf)
+            }
+        }
     }
 }
