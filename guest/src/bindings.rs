d744b9ecf1f1a877f104213c1c011abaef6a59e3
Nate Wilcox <nathan+dev@electriccoin.co>
2023-01-06 15:47:20 -0800

    Drop redundant `handle_` in binding names now that we have named type aliases.

--- guest/src/bindings.rs
+++ guest/src/bindings.rs
@@ -8,25 +8,20 @@ extern "C" {
     pub fn log(ptr: PtrWrite, len: ByteLen);
 
     // Link methods:
-    pub fn link_get_kind(handle_link: HandleLink) -> LinkKind;
-    pub fn link_open_file_reader(handle_link: HandleLink) -> HandleByteReader;
-    pub fn link_open_directory_reader(handle_link: HandleLink) -> HandleDirReader;
-    pub fn link_close(handle_link: HandleLink);
+    pub fn link_get_kind(link: HandleLink) -> LinkKind;
+    pub fn link_open_file_reader(link: HandleLink) -> HandleByteReader;
+    pub fn link_open_directory_reader(link: HandleLink) -> HandleDirReader;
+    pub fn link_close(link: HandleLink);
 
     // ByteReader methods:
-    pub fn byte_reader_read(
-        handle_byte_reader: HandleByteReader,
-        ptr: PtrRead,
-        len: ByteLen,
-    ) -> ByteLen;
-    pub fn byte_reader_close(handle_byte_reader: HandleByteReader);
+    pub fn byte_reader_read(byte_reader: HandleByteReader, ptr: PtrRead, len: ByteLen) -> ByteLen;
+    pub fn byte_reader_close(byte_reader: HandleByteReader);
 
     // DirectoryReader methods:
-    pub fn directory_reader_has_more_entries(handle_directory_reader: HandleDirReader) -> Bool;
-    pub fn directory_reader_open_name_reader(
-        handle_directory_reader: HandleDirReader,
-    ) -> HandleByteReader;
-    pub fn directory_reader_load_link(handle_directory_reader: HandleDirReader) -> HandleLink;
-    pub fn directory_reader_next_entry(handle_directory_reader: HandleDirReader);
-    pub fn directory_reader_close(handle_byte_reader: HandleByteReader);
+    pub fn directory_reader_has_more_entries(directory_reader: HandleDirReader) -> Bool;
+    pub fn directory_reader_open_name_reader(directory_reader: HandleDirReader)
+        -> HandleByteReader;
+    pub fn directory_reader_load_link(directory_reader: HandleDirReader) -> HandleLink;
+    pub fn directory_reader_next_entry(directory_reader: HandleDirReader);
+    pub fn directory_reader_close(byte_reader: HandleByteReader);
 }
