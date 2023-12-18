5e4044f23543297f2164191fc639177df9262843
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-17 17:03:47 -0800

    Implement `pg store insert` to `MemStore`. We need CID transport encoding.

--- cli/src/cmd.rs
+++ cli/src/cmd.rs
@@ -1,3 +1,12 @@
 pub async fn store_insert() -> anyhow::Result<()> {
-    todo!()
+    use pangalactic_store::Store;
+    use pangalactic_store_mem::MemStore;
+
+    let mut store = MemStore::default();
+    let mut r = tokio::io::stdin();
+    let mut w = store.open_writer().await?;
+    tokio::io::copy(&mut r, &mut w).await?;
+    let cid = store.commit_writer(w).await?;
+    println!("{cid:?}");
+    Ok(())
 }
