7a1311d6d08be629b0b6aea3a8362e6a993068d2
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-18 05:29:53 -0800

    `pg store insert`: switch from bare `Store` to `Dagio`.

--- cli/src/cmd.rs
+++ cli/src/cmd.rs
@@ -1,13 +1,14 @@
+use pangalactic_dagio::Dagio;
+use pangalactic_store_mem::MemStore;
+
 pub async fn store_insert() -> anyhow::Result<()> {
     use pangalactic_serialization::AsyncSerialize;
-    use pangalactic_store::Store;
-    use pangalactic_store_mem::MemStore;
 
-    let mut store = MemStore::default();
+    let mut dagio = Dagio::from(MemStore::default());
     let mut r = tokio::io::stdin();
-    let mut w = store.open_writer().await?;
+    let mut w = dagio.open_file_writer().await?;
     tokio::io::copy(&mut r, &mut w).await?;
-    let cid = store.commit_writer(w).await?;
+    let cid = dagio.commit_file_writer(w).await?;
     let mut ser = vec![];
     cid.write_into(&mut ser).await?;
     let enc = pangalactic_b64::encode(ser);
