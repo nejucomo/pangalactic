70e0279493fc286b7054a11c078dc5b89f9afed1
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-22 05:50:18 -0800

    Remove `Display` and `FromStr` for `CidMeta`.

--- layer-cidmeta/src/cidmeta.rs
+++ layer-cidmeta/src/cidmeta.rs
@@ -1,7 +1,5 @@
 use pangalactic_store::Store;
 use serde::{Deserialize, Serialize};
-use std::fmt;
-use std::str::FromStr;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct CidMeta<S>
@@ -42,35 +40,5 @@ where
             cid: self.cid.clone(),
             node_size: self.node_size,
         }
-    }
-}
-
-impl<S> FromStr for CidMeta<S>
-where
-    S: Store,
-    <S as Store>::CID: FromStr<Err = anyhow::Error>,
-{
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> anyhow::Result<Self> {
-        let (cidtext, sizetext) = s
-            .split_once(':')
-            .ok_or_else(|| anyhow::anyhow!("missing ':' in {s:?}"))?;
-        let cid = cidtext.parse()?;
-        let node_size = sizetext.parse()?;
-        Ok(CidMeta { cid, node_size })
-    }
-}
-
-impl<S> fmt::Display for CidMeta<S>
-where
-    S: Store,
-    <S as Store>::CID: fmt::Display,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.cid.fmt(f)?;
-        ':'.fmt(f)?;
-        self.node_size.fmt(f)?;
-        Ok(())
     }
 }
