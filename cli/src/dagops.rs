fe87804c29fc29cdfe73a34b8d45442991de5195
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-25 14:26:28 -0800

    Untested implementation of `pg store tree import`.

--- cli/src/dagops.rs
+++ cli/src/dagops.rs
@@ -1,26 +1,37 @@
 use std::path::{Path, PathBuf};
 
+use async_recursion::async_recursion;
 use either::Either;
-use pangalactic_dagio::Dagio;
+use pangalactic_dagio::{Dagio, FromDag, HostDirectoryFor, ToDag};
 use pangalactic_layer_cidmeta::CidMeta;
 use pangalactic_link::Link;
 use pangalactic_path::AnyPath;
 use pangalactic_store_dirdb::DirDbStore;
+use tokio::io::AsyncRead;
 
 #[derive(Debug, Default)]
 pub struct DagOps(Dagio<DirDbStore>);
 
 pub type AnyPathDo = AnyPath<CidMeta<DirDbStore>>;
+pub type DirectoryDo = HostDirectoryFor<DirDbStore>;
 pub type LinkDo = Link<CidMeta<DirDbStore>>;
 
 impl DagOps {
     pub async fn store_file_put(&mut self) -> anyhow::Result<()> {
-        let mut r = tokio::io::stdin();
-        let mut w = self.0.open_file_writer().await?;
-        tokio::io::copy(&mut r, &mut w).await?;
-        let link = self.0.commit_file_writer(w).await?;
+        let link = self.file_put(tokio::io::stdin()).await?;
         println!("{link}");
         Ok(())
+    }
+
+    async fn file_put<R>(&mut self, r: R) -> anyhow::Result<LinkDo>
+    where
+        R: AsyncRead,
+    {
+        let mut pinr = std::pin::pin!(r);
+        let mut w = self.0.open_file_writer().await?;
+        tokio::io::copy(&mut pinr, &mut w).await?;
+        let link = self.0.commit_file_writer(w).await?;
+        Ok(link)
     }
 
     pub async fn store_file_get(&mut self, link: &LinkDo) -> anyhow::Result<()> {
@@ -31,9 +42,7 @@ impl DagOps {
     }
 
     pub async fn store_dir_empty(&mut self) -> anyhow::Result<()> {
-        use pangalactic_dagio::{HostDirectoryFor, ToDag};
-
-        let hd = HostDirectoryFor::default();
+        let hd = DirectoryDo::default();
         let link = hd.into_dag(&mut self.0).await?;
         println!("{link}");
         Ok(())
@@ -45,8 +54,6 @@ impl DagOps {
         name: &str,
         target: &LinkDo,
     ) -> anyhow::Result<()> {
-        use pangalactic_dagio::{FromDag, HostDirectoryFor, ToDag};
-
         let mut hd = HostDirectoryFor::from_dag(&mut self.0, dir).await?;
         hd.insert(name.to_string(), target.clone())?;
         let link = hd.into_dag(&mut self.0).await?;
@@ -55,8 +62,6 @@ impl DagOps {
     }
 
     pub async fn store_dir_unlink(&mut self, dir: &LinkDo, name: &str) -> anyhow::Result<()> {
-        use pangalactic_dagio::{FromDag, HostDirectoryFor, ToDag};
-
         let mut hd = HostDirectoryFor::from_dag(&mut self.0, dir).await?;
         hd.remove_required(name)?;
         let link = hd.into_dag(&mut self.0).await?;
@@ -65,8 +70,6 @@ impl DagOps {
     }
 
     pub async fn store_dir_list(&mut self, dir: &LinkDo) -> anyhow::Result<()> {
-        use pangalactic_dagio::{FromDag, HostDirectoryFor};
-
         let hd = HostDirectoryFor::from_dag(&mut self.0, dir).await?;
         for (name, link) in hd {
             println!("{link} {name}");
@@ -79,7 +82,34 @@ impl DagOps {
     }
 
     pub async fn store_tree_import(&mut self, source: &Path) -> anyhow::Result<()> {
-        todo!("import {:?}", source.display());
+        let root = self.import_path(source).await?;
+        println!("{root}");
+        Ok(())
+    }
+
+    #[async_recursion(?Send)]
+    async fn import_path(&mut self, p: &Path) -> anyhow::Result<LinkDo> {
+        if p.is_file() {
+            let f = tokio::fs::File::open(p).await?;
+            self.file_put(f).await
+        } else if p.is_dir() {
+            let mut dd = DirectoryDo::default();
+            let mut readdir = tokio::fs::read_dir(p).await?;
+            while let Some(entry) = readdir.next_entry().await? {
+                let childname_ostr = entry.file_name();
+                let childname = childname_ostr
+                    .to_str()
+                    .ok_or_else(|| anyhow::anyhow!("Cannot convert {childname_ostr:?} to utf8"))?;
+                let childlink = self.import_path(&entry.path()).await?;
+                dd.insert(childname.to_string(), childlink)?;
+            }
+            dd.into_dag(&mut self.0).await
+        } else {
+            anyhow::bail!(
+                "cannot convert {:?} which is neither file, nor dir",
+                p.display()
+            );
+        }
     }
 
     pub async fn store_tree_export(&mut self, root: &LinkDo, dest: &Path) -> anyhow::Result<()> {
@@ -104,7 +134,6 @@ impl DagOps {
 
     async fn resolve(&mut self, path: AnyPathDo) -> anyhow::Result<Either<PathBuf, LinkDo>> {
         use either::Either::*;
-        use pangalactic_dagio::{FromDag, HostDirectoryFor};
         use AnyPath::*;
 
         match path {
