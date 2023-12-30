b077dec11c33c3b3ed5c424ad07aec07776c2ad1
Nate Wilcox <nejucomo+dev@gmail.com>
2023-12-30 10:40:19 -0800

    Change `...-dagio` mod names for consistency with trait names.

--- dagio/src/lib.rs
+++ dagio/src/lib.rs
@@ -2,18 +2,18 @@
 #![cfg_attr(doc, feature(async_fn_in_trait))]
 
 mod aliases;
+mod commit;
 mod dagio;
 mod dirimpl;
-mod fromdag;
+mod load;
 mod reader;
-mod todag;
 mod writer;
 
 pub use self::aliases::{DagioHostDirectory, DagioLink};
+pub use self::commit::DagioCommit;
 pub use self::dagio::Dagio;
-pub use self::fromdag::DagioLoad;
+pub use self::load::DagioLoad;
 pub use self::reader::DagioReader;
-pub use self::todag::DagioCommit;
 pub use self::writer::DagioWriter;
 
 #[cfg(test)]
