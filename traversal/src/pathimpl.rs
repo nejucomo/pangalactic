d8bc4bc7c2657d2b63dbad3347ecd345fa4954df
Nate Wilcox <nejucomo+dev@gmail.com>
2024-01-12 13:18:34 -0800

    Sort directory contents in `TraversableDag`.

--- traversal/src/pathimpl.rs
+++ traversal/src/pathimpl.rs
@@ -16,33 +16,33 @@ impl TraversableDag for PathBuf {
 
     fn children(&self) -> Self::ChildrenFut {
         if self.is_dir() {
-            Box::pin(pathbuf_dir_to_future(self.clone()))
+            Box::pin(get_sorted_children(self.clone()))
         } else {
+            // A non-directory has no children:
             let stream: ChildStream = Box::pin(tokio_stream::empty());
-
             Box::pin(std::future::ready(Ok(stream)))
         }
     }
 }
 
-fn pathbuf_dir_to_future(p: PathBuf) -> impl Future<Output = IOResult<ChildStream>> + 'static {
-    use futures::FutureExt;
-    use tokio::fs::read_dir;
+async fn get_sorted_children(path: PathBuf) -> anyhow::Result<ChildStream> {
+    use anyhow::Context;
 
-    read_dir(p.clone()).map(move |rdres| {
-        rdres
-            .map(read_dir_to_pathbuf_stream)
-            .map_err(|e| anyhow::Error::from(e).context(format!("for path {:?}", p.display())))
-    })
+    let ctx = format!("with path {:?}", path.display());
+    get_sorted_children_without_context(path).await.context(ctx)
 }
 
-fn read_dir_to_pathbuf_stream(rd: tokio::fs::ReadDir) -> ChildStream {
+async fn get_sorted_children_without_context(path: PathBuf) -> anyhow::Result<ChildStream> {
     use tokio_stream::wrappers::ReadDirStream;
     use tokio_stream::StreamExt;
 
-    Box::pin(ReadDirStream::new(rd).map(|entry_res| {
-        entry_res
-            .map(|entry| entry.path())
-            .map_err(anyhow::Error::from)
-    }))
+    let rd = tokio::fs::read_dir(path).await?;
+    let res: anyhow::Result<Vec<PathBuf>> = ReadDirStream::new(rd)
+        .map(|res| res.map_err(anyhow::Error::from))
+        .map(|entres| entres.map(|entry| entry.path()))
+        .collect()
+        .await;
+    let mut v = res?;
+    v.sort();
+    Ok(Box::pin(tokio_stream::iter(v.into_iter().map(Ok))))
 }
