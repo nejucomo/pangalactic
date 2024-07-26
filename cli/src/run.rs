7e7e8bee977d30f1d5df11dd7bbdd183504c816d
user <nobody@nowhere>
2024-07-26 10:59:10 -0700

    Enable tracing subscriber in cli; `trace::debug` cli options.

--- cli/src/run.rs
+++ cli/src/run.rs
@@ -2,5 +2,17 @@ use crate::options::Options;
 
 pub async fn run() -> anyhow::Result<()> {
     let opts = Options::parse();
+    init_logging()?;
+    tracing::debug!(options = ?opts);
     opts.run().await
+}
+
+fn init_logging() -> anyhow::Result<()> {
+    use tracing::Level;
+
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .with_writer(std::io::stderr)
+        .try_init()
+        .map_err(|e| anyhow::anyhow!(e))
 }
