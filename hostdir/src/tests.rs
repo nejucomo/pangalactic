fb2f5a3e9d30575efa57801ac11f23cb1f120b1c
Nate Wilcox <nejucomo+dev@gmail.com>
2024-08-03 09:01:45 -0700

    Comment out un-refactored crates higher in the stack; fix tests.

--- hostdir/src/tests.rs
+++ hostdir/src/tests.rs
@@ -1,6 +1,5 @@
 use crate::HostDirectory;
 use pangalactic_hash::Hash;
-use pangalactic_layer_cidmeta::CidMeta;
 use pangalactic_link::Link;
 use pangalactic_linkkind::LinkKind;
 use pangalactic_serialization::check_serialize_then_deserialize_equality;
@@ -14,21 +13,17 @@ fn test_empty_directory() -> anyhow::Result<()> {
 
 #[test]
 fn test_directory() -> anyhow::Result<()> {
-    fn make_cid(bytes: &[u8]) -> CidMeta<Hash> {
-        CidMeta::new(Hash::of(bytes), u64::try_from(bytes.len()).unwrap())
-    }
-
     use LinkKind::*;
 
     let mut d: FLDirectory = FLDirectory::default();
     d.insert(
         "alpha".to_string(),
-        Link::new(File, make_cid(b"a nonempty file")),
+        Link::new(File, Hash::of(b"a nonempty file")),
     )
     .unwrap();
     d.insert(
         "beta".to_string(),
-        Link::new(Dir, make_cid(b"not a real dir")),
+        Link::new(Dir, Hash::of(b"not a real dir")),
     )
     .unwrap();
 
--- link/src/testutil.rs
+++ link/src/testutil.rs
@@ -1,30 +1,7 @@
-use async_trait::async_trait;
-use pangalactic_store::{ContentIdentifier, Store};
+use pangalactic_cid::ContentIdentifier;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
 pub struct FakeKey;
 
 impl ContentIdentifier for FakeKey {}
-
-#[derive(Debug)]
-pub struct FakeStore;
-
-#[async_trait]
-impl Store for FakeStore {
-    type CID = FakeKey;
-    type Reader = tokio::io::Empty;
-    type Writer = tokio::io::Sink;
-
-    async fn open_reader(&self, _: &Self::CID) -> anyhow::Result<Self::Reader> {
-        unimplemented!("open_reader")
-    }
-
-    async fn open_writer(&self) -> anyhow::Result<Self::Writer> {
-        unimplemented!("open_writer")
-    }
-
-    async fn commit_writer(&mut self, _: Self::Writer) -> anyhow::Result<Self::CID> {
-        unimplemented!("commit_writer")
-    }
-}
