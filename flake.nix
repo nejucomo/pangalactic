f91a49f040aad66c02cec0a44dca9a595d5cdbf1
nobody <nobody@nowhere.net>
2025-06-14 22:56:48 -0600

    Add a `flake.nix` with `nix develop` environment; fix two `error: lifetime flowing from input to output with different syntax can be confusing`.

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Rust dev shell with native and wasm32-unknown-unknown support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = [ rustToolchain ]; };
+      }
+    );
+}
