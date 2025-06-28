3431608179e73dfdef93ed112230c4640d68e98a
nobody <nobody@nowhere.net>
2025-06-28 11:50:32 -0700

    Remove vestigial unlinked/untested `pg-seed` crate.

--- crates/cli/pg-seed/src/bin/pg-seed.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-use pangalactic_cli_seed::SeedApplication;
-use pangalactic_runopt::Application;
-
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
-    SeedApplication::run_main().await
-}
